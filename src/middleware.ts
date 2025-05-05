a1c59a17ebe2737c75b0d3eddb265030a02ba7e8
Yoonhwi <ush0105@naver.com>
2025-05-06 02:46:44 +0900

    refactor: use next-intl's built-in 'locale' type and 'hasLocale'

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,9 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import { Locale } from "./lib/i18n/locale";
 import createMiddleware from "next-intl/middleware";
 import { routing } from "./lib/i18n/routing";
 import { getPathname } from "./lib/i18n/navigation";
+import { hasLocale, Locale } from "next-intl";
 
 const intlMiddleware = createMiddleware(routing);
 
@@ -11,7 +11,7 @@ export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
   const locale = pathname.split("/")[1] as Locale;
 
-  if (!locale || !routing.locales.includes(locale)) {
+  if (!locale || !hasLocale(routing.locales, locale)) {
     const redirectUrl = new URL(
       getPathname({ href: pathname, locale: routing.defaultLocale }),
       request.nextUrl.origin
